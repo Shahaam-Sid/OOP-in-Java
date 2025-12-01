0d6522116f9d6696f40eb8c68c2bf23590b61b9e
Shahaam-Sid <muhammadgc821@gmail.com>
2025-12-01 17:40:09 +0500

    Updated CreditCard class and added main in it

--- CreditCard/CreditCard.java
+++ CreditCard/CreditCard.java
@@ -16,8 +16,8 @@ public class CreditCard {
     public String getCustomerName() {return customerName;}
     public String getBankName() {return bankName;}
     public String getAccountId() {return accountId;}
-    public int getLim() {return lim;}
-    public double getBal() {return bal;}
+    public int getLimit() {return lim;}
+    public double getBalance() {return bal;}
     public boolean charge(double credit) {
         if ((credit + bal) > lim) {
             return false;
@@ -27,13 +27,49 @@ public class CreditCard {
         }
     }
     public void makePayment(double bill) {
-        bal -= bill;
+        if (bill > 0) {
+            bal -= bill;
+        }
     }
-    public static void printSummary(CreditCard card) {
-        System.out.println("Customer Name: " + card.customerName);
-        System.out.println("Bank: " + card.bankName);
-        System.out.println("ID: " + card.accountId);
-        System.out.println("Balance: " + card.bal);
-        System.out.println("Limit: " + card.lim);
+    public void  updateLimit(int limit) {
+        lim = limit;
     }
+    public void printSummary() {
+        System.out.println("Customer Name: " + customerName);
+        System.out.println("Bank: " + bankName);
+        System.out.println("ID: " + accountId);
+        System.out.println("Balance: " + bal);
+        System.out.println("Limit: " + lim);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer Name: " + customerName +
+        "Bank: " + bankName + "ID: " + accountId + 
+        "Balance: " + bal + "Limit: " + lim;
+    }
+    public static void main(String[] args) {
+    CreditCard[] wallet = new CreditCard[3];
+    wallet[0] = new CreditCard("John Bowman", "California Savings",
+                              "5391 0375 9387 5309", 5000);
+    wallet[1] = new CreditCard("John Bowman", "California Federal",
+                              "3485 0399 3395 1954", 3500);
+    wallet[2] = new CreditCard("John Bowman", "California Finance",
+                              "5391 0375 9387 5309", 2500, 300);
+
+    for (int val = 1; val <= 16; val++) {
+        wallet[0].charge(3 * val);
+        wallet[1].charge(2 * val);
+        wallet[2].charge(val);
+    }
+
+    for (CreditCard card : wallet) {
+        card.printSummary();
+        while (card.getBalance() > 200.0) {
+            card.makePayment(200);
+            System.out.println("New balance = " + card.getBalance());
+        }
+    }
+    }        
+
 }
